32837d9cbb4884d0edc5be4385bbf4205acc4ff3
Bollie <bitbucket@bolukan.nl>
2018-12-22 17:51:12 +0100

    Fingerprint BME280 CRC32

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,15 @@
 #include <SPI.h>                      // need for buggy BME280
 
 #if defined(ESP8266)
-	#include <ESP8266WiFi.h>
+  #include <ESP8266WiFi.h>
 #elif defined(ESP32)
-	#include <WiFi.h>
+  #include <WiFi.h>
 #endif
 
 #include <MySQL_Connection.h>
 #include <MySQL_Cursor.h>
 #include <BME280I2C.h>
+#include <CRC32.h>
 
 const long SERIAL_BAUD = 115200;
 const int SENSOR_INTERVAL_MILLIS = 60000;
@@ -19,13 +20,13 @@ const char INSERT_SQL[] = "INSERT INTO `sensor`.`BME280` (`sensorid`, `timestamp
 #include "secrets.h"
 #ifndef SECRETS_H
 	#define SECRETS_H
-	const char WIFI_SSID[]              = "WIFI_SSID_HERE";
-	const char WIFI_PASSWORD[]          = "WIFI_PASSWORD_HERE";
+	const char WIFI_SSID[] = "WIFI_SSID_HERE";
+	const char WIFI_PASSWORD[] = "WIFI_PASSWORD_HERE";
 
-	const char MYSQL_USER[]             = "MYSQL_USER_HERE";
-	const char MYSQL_PASSWORD[]         = "MYSQL_PASSWORD_HERE";
-	IPAddress MYSQL_SERVERIP(192, 168, 1, 100);       // MySQL server IP
-	const int MYSQL_SERVERPORT          = 3306;       // default 3306
+	const char MYSQL_USER[] = "MYSQL_USER_HERE";
+	const char MYSQL_PASSWORD[] = "MYSQL_PASSWORD_HERE";
+	IPAddress MYSQL_SERVERIP(192, 168, 1, 100); // MySQL server IP
+	const int MYSQL_SERVERPORT = 3306;          // default 3306
 #endif
 
 /*
@@ -51,10 +52,11 @@ char query[200];
  */
 
 #if defined(ESP8266)
-  uint32_t sensorID = ESP.getChipId();
+	uint32_t deviceID = ESP.getChipId();
 #elif defined(ESP32)
- 	uint32_t sensorID = ESP.getEfuseMac() & 0xffffff;
+	uint32_t deviceID = ESP.getEfuseMac() & 0xffffff;
 #endif
+uint32_t sensorID = deviceID;
 
 BME280I2C::Settings settings(
 	BME280::OSR_X1,
@@ -64,8 +66,8 @@ BME280I2C::Settings settings(
 	BME280::StandbyTime_1000ms,
 	BME280::Filter_Off,
 	BME280::SpiEnable_False,
-	BME280I2C::I2CAddr_0x76 // I2C address. I2C specific.
-);
+	BME280I2C::I2CAddr_0x76           // I2C address. I2C specific.
+	);
 
 BME280I2C bme(settings);
 
@@ -75,13 +77,15 @@ BME280I2C bme(settings);
 
 // Station connected
 // WiFiEventStationModeConnected: String ssid, uint8 bssid[6], uint8 channel
-void onSTAConnected(WiFiEventStationModeConnected stationInfo) {
-	Serial.printf("Connected to %s\n", stationInfo.ssid.c_str ());
+void onSTAConnected(WiFiEventStationModeConnected stationInfo)
+{
+	Serial.printf("Connected to %s\n", stationInfo.ssid.c_str());
 }
 
 // Got IP
 // WiFiEventStationModeGotIP: IPAddress ip, IPAddress mask, IPAddress gw
-void onSTAGotIP(WiFiEventStationModeGotIP ipInfo) {
+void onSTAGotIP(WiFiEventStationModeGotIP ipInfo)
+{
 	Serial.printf("Connected: %s\n", WiFi.status() == WL_CONNECTED ? "yes" : "no");
 	Serial.printf("Got IP: %s\n", ipInfo.ip.toString().c_str());
 }
@@ -89,7 +93,8 @@ void onSTAGotIP(WiFiEventStationModeGotIP ipInfo) {
 // Station disconnected
 // WiFiEventStationModeDisconnected: String ssid, uint8 bssid[6], WiFiDisconnectReason reason
 // https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/src/ESP8266WiFiType.h
-void onSTADisconnected(WiFiEventStationModeDisconnected disconnectInfo) {
+void onSTADisconnected(WiFiEventStationModeDisconnected disconnectInfo)
+{
 	Serial.printf("Disconnected from SSID: %s\n", disconnectInfo.ssid.c_str());
 	Serial.printf("Reason: %d\n", disconnectInfo.reason);
 	// reboot
@@ -112,10 +117,10 @@ void connectToMySQL()
 	if (WiFi.status() != WL_CONNECTED) return;
 	// if (!conn.connected()) { Serial.println("conn not connected"); return; }
 	Serial.println(F("Starting MYSQL connection"));
-	Serial.printf("SQL: Connecting to %s as %s ... : ", MYSQL_SERVERIP.toString().c_str(), (char *)MYSQL_USER);
+	Serial.printf("SQL: Connecting to %s as %s ... : ", MYSQL_SERVERIP.toString().c_str(), (char*)MYSQL_USER);
 
 	// boolean connect(IPAddress server, int port, char *user, char *password);
-	if (conn.connect(MYSQL_SERVERIP, MYSQL_SERVERPORT, (char *)MYSQL_USER, (char *)MYSQL_PASSWORD)) {
+	if (conn.connect(MYSQL_SERVERIP, MYSQL_SERVERPORT, (char*)MYSQL_USER, (char*)MYSQL_PASSWORD)) {
 		Serial.println(F("OK. CONNECTED"));
 		// create MySQL cursor object
 		cursor = new MySQL_Cursor(&conn);
@@ -145,7 +150,7 @@ void setup()
 
 	// BME280
 	Serial.println(F("Starting BME280"));
-	Serial.printf("SensorID (based on ESP): %06X\n", sensorID);
+	Serial.printf("deviceID (based on ESP): %06X\n", deviceID);
 	Wire.begin();
 	if (!bme.begin()) {
 		Serial.println(F("Could not find BME280 sensor"));
@@ -160,7 +165,18 @@ void setup()
 		ESP.restart();
 	}
 
-	previousMillis = millis() - (SENSOR_INTERVAL_MILLIS - 10000); // 10 seconds before first measurement
+  uint8_t test[32];
+  // Calculate a checksum one byte at a time.
+  CRC32 crc;
+  uint32_t checksum = CRC32::calculate(bme.compensationParameters(test), 32);
+  Serial.printf("SensorID (CRC32): 0x%08X  Hash(CRC32) of ", checksum);
+  for (int i=0; i<32; i++)
+  {
+    Serial.printf("%02X ", test[i]);
+  }
+  Serial.print("\n");
+
+	previousMillis = millis() - (SENSOR_INTERVAL_MILLIS - 10000);      // 10 seconds before first measurement
 }
 
 void loop()
@@ -181,12 +197,12 @@ void loop()
 				BME280::PresUnit presUnit(BME280::PresUnit_hPa);
 				bme.read(pres, temp, hum, tempUnit, presUnit);
 
-				uint32_t hum100  = (uint32_t)(hum * 100);
+				uint32_t hum100 = (uint32_t)(hum * 100);
 				uint32_t pres100 = (uint32_t)(pres * 100);
 				int32_t temp100 = (int32_t)(temp * 100);
 				sprintf(query, INSERT_SQL, sensorID, hum100, pres100, temp100);
 				Serial.printf("Query %s\n", query);
-				cursor->execute(query);
+				//cursor->execute(query);
 				//conn.close();
 				//dbIsConnected = false;
 			}
